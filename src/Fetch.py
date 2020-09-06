b951c93716ba175d6c636f327c4d55fae27f7970
angelinag <andji.gash@gmail.com>
2020-09-06 22:33:59 +0300

    Added pagination fetching

--- src/Fetch.py
+++ src/Fetch.py
@@ -5,7 +5,13 @@ from bs4 import BeautifulSoup
 class Fetch:
     def __init__(self):
         self.URL = 'https://www.yelp.com/biz/american-airlines-irving'
+        self.pagenum = 0
+
+    def getnextpageurl(self):
+        querystring = '?start=' + str(self.pagenum * 20)
+        self.pagenum = self.pagenum + 1
+        return self.URL + querystring
 
     def fetchdom(self):
-        page = requests.get(self.URL)
+        page = requests.get(self.getnextpageurl())
         return BeautifulSoup(page.content, 'html.parser')
--- start.py
+++ start.py
@@ -2,14 +2,30 @@ from src.Fetch import Fetch
 from src.Parse import Parse
 from src.Export import Export
 
-f1 = Fetch()
-p1 = Parse(f1.fetchdom())
+f = Fetch()
+stars = []
+names = []
+texts = []
+dates = []
+
+while True:
+    p = Parse(f.fetchdom())
+    morestars = p.getstars()
+    morenames = p.getnames()
+    moretexts = p.gettexts()
+    moredates = p.getdates()
+    iscomplete = bool(morestars and morenames and moretexts and moredates)
+    if iscomplete is False:
+        break
+    stars = stars + morestars
+    names = names + morenames
+    texts = texts + moretexts
+    dates = dates + moredates
+
 
 e1 = Export()
-e1.populatecolumn(p1.getstars())
-e1.populatecolumn(p1.getnames())
-e1.populatecolumn(p1.gettexts())
-e1.populatecolumn(p1.getdates())
+e1.populatecolumn(stars)
+e1.populatecolumn(names)
+e1.populatecolumn(texts)
+e1.populatecolumn(dates)
 e1.endexport()
-
-
